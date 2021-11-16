8985734dbe0c4e505355e923ef7b04a4f5114b3b
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2021-11-16 14:36:12 +0900

    Clean up and support YouTube videos

--- image_extractor.py
+++ image_extractor.py
@@ -1,34 +1,55 @@
 import cv2
 import random
+import pafy
+
+# font settings
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+FONT_COLOR = (255, 255, 255)
+FONT_SCALE = 1
+FONT_THICKNESS = 2
+POSITION = (100, 100)
+LINE_TYPE = cv2.LINE_AA
 
 
-def generate_captioned_image():
+def generate_captioned_image(video, start_time, end_time, sentence):
+    image = extract_image_from_range(video, start_time, end_time)
+    captioned_image = add_text(image, sentence)
+    return captioned_image
 
-def extract_image_from_range():
+
+def extract_image_from_range(video, start_time, end_time):
+    extract_time = random.randint(start_time, end_time)
+    video.set(cv2.CAP_PROP_POS_MSEC, extract_time)
+    if (video.isOpened()):
+        ret, frame = video.read()
+        if ret == True:
+            return frame
+
+    return None
+
 
 def add_text(image, text):
+    height, width, _ = image.shape
+    pos = (20, height - 20,)
+    return cv2.putText(image, text, pos, FONT, FONT_SCALE, FONT_COLOR, FONT_THICKNESS, LINE_TYPE, False)
 
 
 if __name__ == "__main__":
-    # set up text constants
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    vid = cv2.VideoCapture('samplevid.mp4')
-
-    time_range = (1000, 2000) # in ms
+    video_path = 'samplevid.mp4'
+    start_time = 1000
+    end_time = 2000
     sentence = 'hello world'
 
-    extract_time = random.randint(time_range[0], time_range[1])
+    # read video from file
+    video = cv2.VideoCapture(video_path)
 
-    vid.set(cv2.CAP_PROP_POS_MSEC, extract_time)
+    # read video from YouTube (더 오래 걸리지만, 영상을 다운 받을 수 없다면 사용 가능)
+    #
+    # url = 'https://www.youtube.com/watch?v=IZIywuXTmJk&ab_channel=%EB%94%A9%EA%B3%A0%EB%AE%A4%EC%A7%81%2Fdingomusic'
+    # youtube_video = pafy.new(url)
+    # best_resolution_video = youtube_video.getbest(preftype='mp4')
+    # video = cv2.VideoCapture(best_resolution_video.url)
 
+    image = generate_captioned_image(video, start_time, end_time, sentence)
 
-    if (vid.isOpened()):
-        ret, frame = vid.read()
-        if ret == True:
-            captioned_image = cv2.putText(frame, sentence, (50,100), font, 3, (255,255,255), 2, cv2.LINE_AA)
-            cv2.imwrite('captured_image.jpg', frame)
-
-    vid.release()
-    cv2.destroyAllWindows()
-
+    cv2.imwrite('captured_image.jpg', image)
